3fe81c3754b6e93405ee2302b72d7193d3d95503
Julius Krüger Madsen <1230150922@protonmail.com>
2023-09-25 18:41:02 +0200

    Added Models folder and Note.cs

---
+++ Models/Note.cs
@@ -0,0 +1,9 @@
+﻿namespace Models;
+
+public class Note
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime DateCreated { get; set; }
+}
